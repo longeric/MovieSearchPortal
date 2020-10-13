f6cdedf618de1e8e2e3510c6488a66779973e62a
Xuzijian <zix10@pitt.edu>
2020-10-12 20:51:21 -0400

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchHomeComponent } from './components/search-home/search-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchHomeComponent
   ],
   imports: [
     BrowserModule,
